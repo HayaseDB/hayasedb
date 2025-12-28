903a490a8ca157cf5cbf9a4c4cad69e810c1f469
AIO <Sebastian-Stepper@gmx.de>
2025-12-28 17:43:01 +0100

    chore: add needed ui components

---
+++ web/app/components/ui/alert/Alert.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import type { HTMLAttributes } from 'vue'
+  import type { AlertVariants } from '.'
+  import { cn } from '@/lib/utils'
+  import { alertVariants } from '.'
+
+  interface Props {
+    variant?: AlertVariants['variant']
+    class?: HTMLAttributes['class']
+  }
+
+  const props = withDefaults(defineProps<Props>(), {
+    variant: 'default',
+    class: undefined,
+  })
+</script>
+
+<template>
+  <div role="alert" data-slot="alert" :class="cn(alertVariants({ variant }), props.class)">
+    <slot />
+  </div>
+</template>
---
+++ web/app/components/ui/alert/AlertTitle.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import type { HTMLAttributes } from 'vue'
+  import { cn } from '@/lib/utils'
+
+  interface Props {
+    class?: HTMLAttributes['class']
+  }
+
+  const props = withDefaults(defineProps<Props>(), {
+    class: undefined,
+  })
+</script>
+
+<template>
+  <h5
+    data-slot="alert-title"
+    :class="cn('col-start-2 line-clamp-1 min-h-4 font-medium tracking-tight', props.class)"
+  >
+    <slot />
+  </h5>
+</template>
