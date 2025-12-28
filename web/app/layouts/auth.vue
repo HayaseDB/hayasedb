8817b831949dbef1c9c800875111c97cc7ae3e50
AIO <Sebastian-Stepper@gmx.de>
2025-12-28 22:26:02 +0100

    refactor: size of logo

--- web/app/layouts/auth.vue
+++ web/app/layouts/auth.vue
@@ -6,11 +6,11 @@
   <div class="bg-background text-foreground grid min-h-svh lg:grid-cols-2">
     <div class="flex flex-col gap-4 p-6 md:p-10">
       <div class="flex justify-center md:justify-start">
-        <NuxtLink to="/" class="flex items-center gap-2 text-lg font-semibold md:gap-3 md:text-xl">
+        <NuxtLink to="/" class="flex items-center gap-1 text-lg font-semibold md:gap-2 md:text-2xl">
           <img
             src="~/assets/svg/icon-rounded.svg"
             alt="HayaseDB"
-            class="size-8 rounded-lg md:size-10"
+            class="size-8 rounded-lg md:size-12"
           />
           HayaseDB
         </NuxtLink>
