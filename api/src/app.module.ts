61d9de7c1b1d408d9639f2cdf331ab9b356928a7
AIO <Sebastian-Stepper@gmx.de>
2025-12-28 03:34:55 +0100

    feat: mail setup

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,8 +3,20 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { HealthModule } from './health/health.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { MailModule } from './modules/mail/mail.module';
+import { SessionsModule } from './modules/sessions/sessions.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, HealthModule],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    HealthModule,
+    MailModule,
+    UsersModule,
+    SessionsModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
