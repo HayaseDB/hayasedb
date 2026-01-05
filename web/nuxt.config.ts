29102914454c74cac58967b7638fd57cbe73e643
AIO <Sebastian-Stepper@gmx.de>
2026-01-05 17:27:26 +0100

    chore: add missing shadcn components

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -61,6 +61,8 @@ export default defineNuxtConfig({
           firstName: 'string',
           lastName: 'string',
           role: 'string',
+          profilePicture:
+            '{ id: string, bucket: string, key: string, originalName: string, mimeType: string, size: number, etag: string | null, url: string | null } | null',
         },
         dataResponsePointer: '/',
       },
