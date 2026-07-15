a8f0d7d29c400908aa71c734893ee61e6a91cf4f
Sebastian Stepper <Sebastian-Stepper@gmx.de>
2026-07-15 13:33:56 +0200

    fix

--- packages/config/eslint/index.ts
+++ packages/config/eslint/index.ts
@@ -17,33 +17,26 @@ export const ignores: ConfigArray = tseslint.config({
   ],
 })
 
+const rules: ConfigArray = tseslint.config({
+  name: 'hayasedb/rules',
+  rules: {
+    'no-undef': 'off',
+  },
+})
+
 export const base: ConfigArray = tseslint.config(
   ...ignores,
   {
     name: 'hayasedb/base',
     files: SCRIPT_FILES,
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    rules: {
-      'no-undef': 'off',
-    },
   },
+  ...rules,
   prettier,
 )
 
-export const vue: ConfigArray = tseslint.config({
-  name: 'hayasedb/vue-ts-parser',
-  files: ['**/*.vue'],
-  languageOptions: {
-    parserOptions: {
-      parser: tseslint.parser,
-    },
-  },
-})
-
-export function nuxt<T extends { append: (...configs: unknown[]) => R }, R>(
-  withNuxt: (...configs: unknown[]) => T,
-): R {
-  return withNuxt(...base).append(...vue)
+export function nuxt<T>(withNuxt: (...configs: unknown[]) => T): T {
+  return withNuxt(...ignores, ...rules, prettier)
 }
 
 export default base
